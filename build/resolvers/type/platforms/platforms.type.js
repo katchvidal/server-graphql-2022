584f8188ce5cfe5e3c623f35aa278eecb7beda1b
Katch Vidal <katchvidal@gmail.com>
2022-06-16 16:57:58 -0500

    cleaning three

--- build/resolvers/type/platforms/platforms.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolverPlatformType = {
-    // Debe tener nombre del tipo de objeto que haz especificado
-    Platform: {
-        active: (parent) => (parent.active !== false) ? true : false
-    }
-};
-exports.default = resolverPlatformType;
--- build/resolvers/type/products/products.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolverProductsType = {
-    // El nombre debe ser el tipo de objeto referenciado en los schemas
-    Product: {
-        screenshoot: (parent) => parent.shortScreenshots
-    }
-};
-exports.default = resolverProductsType;
